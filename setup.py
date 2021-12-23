7962851f27708993b7aab8a89384c903a789a0fc
Deepak Mishra <deepak123.dm98@gmail.com>
2021-12-23 20:01:34 +0530

    intial

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="docker-heroku",
+    version="0.0.3",
+    description="Freelancing Project",
+    author="Deepak Mishra",
+    packages=find_packages(),
+    license="MIT"
+)
